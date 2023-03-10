bc8a7e2d6752e148ee507da8e1cf270f5f165c20
kmivehchi-xRoird <kammivehchi@gmail.com>
2023-03-09 19:27:39 -0800

    follow along link

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,17 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+async function startWebcam() {
+   try {
+
+      const mediaStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+      console.log(mediaStream);
+      video.srcObject = mediaStream;
+
+      video.play();
+   } catch (e) {
+      console.error(e, "something went wrong")
+   }
+}
+startWebcam()
